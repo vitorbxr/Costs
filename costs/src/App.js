a2cab325742af521d9475bde0adc8b2642caaeaa
vitorbxr <vitor.bxr@gmail.com>
2022-12-07 10:23:32 +0000

    criado form slect

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,29 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Company from './components/pages/Company';
+import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/Contact">Contato</Link>
-        <Link to="/Company">Empresa</Link>
-        <Link to="/NewProject">Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass="min-height">
       <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/company' element={<Company />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/newproject' element={<NewProject />} />
         </Routes>
         </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
---
+++ costs/src/components/pages/Projects.js
@@ -0,0 +1,8 @@
+function Projects () {
+  return (
+    <>
+      <h1>Projetos</h1>
+    </>
+  )
+}
+export default Projects
