d9bff26840f36d78e1f2af53ced72e369df7363d
vitorbxr <vitor.bxr@gmail.com>
2022-12-07 17:18:17 +0000

    criando dashboard do projeto

--- costs/src/components/pages/NewProject.js
+++ costs/src/components/pages/NewProject.js
@@ -23,7 +23,7 @@ function NewProject () {
       .then((data) => {
         console.log(data)
         //redirect
-        navigate('/Projects',{message: 'Projeto criado com sucesso!'})
+        navigate('/Projects', { state: { message: 'Projeto criado com sucesso!' } })
       })
       .catch((err) => console.log(err))
   }
--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,8 +1,28 @@
+import { useLocation } from "react-router-dom"
+import Message from "../layout/Message"
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+
 function Projects () {
+
+  const location = useLocation()
+    let message = ''
+  if (location.state) {
+    message = location.state.message  
+  }
+
   return (
-    <>
-      <h1>Projetos</h1>
-    </>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/NewProject" text="Criar Projeto" />
+      </div>
+      {message && <Message type="sucess" msg={message} />}      
+      <Container customClass='start'>
+        <p>Projetos</p>
+      </Container>
+    </div>
   )
 }
 export default Projects
